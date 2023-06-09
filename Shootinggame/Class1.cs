71dad517c6479f9f11ae15ac77e18733d36213e1
R-aliyeva <rufanefa@code.edu.az>
2023-06-09 16:24:27 +0400

    project added

---
+++ Shootinggame/Class1.cs
@@ -0,0 +1,132 @@
+﻿using Shootinggame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Shootinggame
+
+
+{
+
+    public class InsufficientBulletCountException : Exception
+    {
+       public InsufficientBulletCountException() : base("you can't shoot") 
+        {
+        
+        }
+
+    }
+    public class InvalidBulletCountException : Exception
+
+    {
+        public InvalidBulletCountException() : base("Total bullet count can't be zero or less than zero") { }
+
+    }
+    public abstract class Gun
+    {
+        public int TotalBulletCount { get; set; }
+        public int CurrentBulletCount { get; set; }
+        public Gun(int totalbulletcount)
+        {
+
+            try
+            {
+                if (totalbulletcount <= 0) { throw new InvalidBulletCountException(); }
+                TotalBulletCount = totalbulletcount;
+            }
+            catch (InvalidBulletCountException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        public virtual void Shoot()
+        {
+            CurrentBulletCount = TotalBulletCount;
+            try
+            {
+                if (CurrentBulletCount == 0)
+                { throw new InsufficientBulletCountException(); }
+                CurrentBulletCount--;
+                Console.WriteLine(this.ToString() + " gun is shooting...");
+                Console.WriteLine(this.ToString() + " current bullet count is " + CurrentBulletCount);
+            }
+            catch (InsufficientBulletCountException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public virtual void Reload()
+
+        {
+            
+            CurrentBulletCount=TotalBulletCount;
+
+            Console.WriteLine(this.ToString() + " gun is reloading " + CurrentBulletCount);
+        }
+
+    }
+    public interface IGun
+    {
+        public void Zoom();
+
+
+    }
+    public class M4 : Gun
+    {
+        public M4(int totalbulletcount) : base(totalbulletcount)
+        {
+        }
+  
+
+    }
+
+
+}
+public class AK47 : Gun
+{
+
+
+    public AK47(int totalbulletcount) : base(totalbulletcount)
+    {
+
+    }
+    
+  
+
+}
+public class P92 : Gun, IGun
+{
+
+    public P92(int totalbulletcount) : base(totalbulletcount)
+    {
+
+
+    }
+   
+    public void Zoom()
+    {
+        Console.WriteLine(this.ToString() + " is zooming");
+    }
+}
+public class AWP : Gun, IGun
+{
+    public AWP(int totalbulletcount) : base(totalbulletcount) { }
+   
+
+
+    public void Zoom()
+    {
+        Console.WriteLine(this.ToString() + " is zooming");
+    }
+}
+
+
+
---
+++ Shootinggame/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Shootinggame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AK47 gun1 = new AK47(30);
+            M4 gun2 = new M4(30);
+            P92 gun3 = new P92(60);
+            AWP gun4 = new AWP(10);
+            //Console.WriteLine(gun1.TotalBulletCount);
+            Console.WriteLine("        AK47     ");
+            gun1.Shoot();
+             gun1.Reload();
+            Console.WriteLine("       M4     ");
+            gun2.Shoot();
+            gun2.Reload();
+            Console.WriteLine("       P92     ");
+            gun3.Shoot();
+            gun3.Reload();
+           gun3.Zoom();
+            Console.WriteLine("       AWP     ");
+            gun4.Shoot();
+           gun4.Reload();
+           gun4.Zoom();
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
